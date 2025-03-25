34670ae044105525a5f703f1f9882d7618bba810
naufal <askpradana@gmail.com>
2025-03-26 02:50:26 +0700

    fix: next build store redux

--- src/app/store/storeProvider.tsx
+++ src/app/store/storeProvider.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 import { configureStore } from '@reduxjs/toolkit';
 import { useSelector, useDispatch, TypedUseSelectorHook } from 'react-redux';
 import githubUserReducer from '@/app/store/profileProvider';
+import { Provider } from 'react-redux';
 
 export const store = configureStore({
     reducer: {
@@ -13,3 +16,7 @@ export type AppDispatch = typeof store.dispatch;
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export function StoreProvider({ children }: { children: React.ReactNode }) {
+  return <Provider store={store}>{children}</Provider>;
+}
